37fb4269e2ee8821fa689bc97174c8659edb2a67
kbrooks10 <55714190+kbrooks10@users.noreply.github.com>
2019-10-31 17:19:47 -0700

    Update sketch.js

--- Wednesday/Processing/empty-example/sketch.js
+++ Wednesday/Processing/empty-example/sketch.js
@@ -1,12 +1,4 @@
 function setup() {
   createCanvas(480, 120);
   background(204);
-  }
-  function draw() {
-  if (mouseIsPressed) {
-  fill(0);
-  } else {
-  fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-  }
+}
